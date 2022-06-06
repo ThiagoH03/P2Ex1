71e2eaf5a63f9ce0bb1a6185238fc35e5a5939dd
ThiagoH03 <thiagohenriq03@outlook.com>
2022-06-06 16:46:06 +0000

    Questão feita

---
+++ Questao1.java
@@ -0,0 +1,12 @@
+public class Questao1{
+    public static void main(String[] args) {
+        int segundaSemana = 5;
+        int terceiraSemana =  2;
+        int quartaSemana =  7;
+
+        System.out.println("Valores da semana:");
+        System.out.println("Segunda: " + segundaSemana +".");
+        System.out.println("Terceira: " + terceiraSemana + ".");
+        System.out.println("Quarta: " + quartaSemana + ".");
+    }
+}
